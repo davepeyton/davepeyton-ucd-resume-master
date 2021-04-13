7ea3f060bb1f4cb864aecac9e02a85c85054fca2
DAVE PEYTON <davepeyton@DAVEs-MacBook-Pro.local>
2021-04-13 15:40:06 +0100

    github user info render

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
             lat: 46.619261,
@@ -7,20 +7,20 @@ function initMap() {
         }
     });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [
+    let locations = [
         { lat: 40.785091, lng: -73.968285 },
         { lat: 41.084045, lng: -73.874245 },
         { lat: 40.754932, lng: -73.984016 }
     ];
 
-    var markers = locations.map(function(location, i) {
+    let markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
